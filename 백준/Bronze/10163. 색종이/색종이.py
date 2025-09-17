daccd40c7cd2027847e8755f2ee49d5c9aaf9992
sohnsw95 <djfzmsdl95@gmail.com>
2025-09-17 21:45:45 +0900

    [Bronze I] Title: 색종이, Time: 504 ms, Memory: 116696 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,24 +1,29 @@
 # 색종이의 장 수
 N = int(input())
 # 색종이를 놓을 공간 선언
-grid = [[0] * 101 for _ in range(101)]
+grid = [[0] * 1001 for _ in range(1001)]
+
+# 실제 범위 체크
+max_x = 0
+max_y = 0
+
 
 # (x,y)시작점, 너비와 높이 입력
 for paper_num in range(1, N+1):
-    x, y, w, h = list(map(int, input().split()))
-
+    x, y, w, h = map(int, input().split())
     for i in range(x, x+w):
-
         for j in range(y, y+h):
-
             grid[j][i] = paper_num
+    max_x = max(max_x, x+w)
+    max_y = max(max_y, y+h)
+
+
+cnt = [0] * (N+1)
+
+for y in range(max_y):
+    for x in range(max_x):
+        if grid[y][x] != 0:
+            cnt[grid[y][x]] += 1
 
 for paper_num in range(1, N + 1):
-    cnt = 0
-
-    for y in range(101):
-        for x in range(101):
-
-            if grid[y][x] == paper_num:
-                cnt += 1
-    print(cnt)
+    print(cnt[paper_num])
