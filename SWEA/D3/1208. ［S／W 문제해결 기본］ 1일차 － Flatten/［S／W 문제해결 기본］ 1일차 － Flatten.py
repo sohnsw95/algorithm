df821f84c3d524e8d8e0b8cddcb8bc2d8a692b3b
sohnsw95 <djfzmsdl95@gmail.com>
2025-10-23 23:24:37 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 69 ms, Memory: 53,632 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -1,15 +1,13 @@
 T = 10
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     dump = int(input())
     boxes = list(map(int, input().split()))
 
-    for _ in range(dump): #dump 횟수만큼 진행한다.
+    for _ in range(dump):
+        boxes.sort()    # 가장 낮은~높은 순으로 정렬
+        boxes[0] += 1             # 가장 낮은 곳에 1 쌓기
+        boxes[-1] -= 1            # 가장 높은 곳에서 1 빼기
 
-        max_idx = boxes.index(max(boxes))
-        min_idx = boxes.index(min(boxes))
-
-        boxes[max_idx] -= 1
-        boxes[min_idx] += 1
-
-    ans = max(boxes) - min(boxes)
+    boxes.sort() # 마지막에 한 번 더 정렬해서 차이 계산
+    ans = boxes[-1] - boxes[0]
     print(f"#{tc} {ans}")
