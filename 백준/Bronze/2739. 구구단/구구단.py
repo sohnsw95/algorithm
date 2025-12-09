d0c6075c65c6d833876048257febdf1899366e9a
sohnsw95 <djfzmsdl95@gmail.com>
2025-12-09 23:48:44 +0900

    [Bronze V] Title: 구구단, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.py
@@ -0,0 +1,15 @@
+'''
+N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
+'''
+
+num = int(input())
+
+print(f"{num} * 1 = {num*1}")
+print(f"{num} * 2 = {num*2}")
+print(f"{num} * 3 = {num*3}")
+print(f"{num} * 4 = {num*4}")
+print(f"{num} * 5 = {num*5}")
+print(f"{num} * 6 = {num*6}")
+print(f"{num} * 7 = {num*7}")
+print(f"{num} * 8 = {num*8}")
+print(f"{num} * 9 = {num*9}")
