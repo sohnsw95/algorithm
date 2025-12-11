7e305090fecd510a2910304e97f0e836c02df2fb
sohnsw95 <djfzmsdl95@gmail.com>
2025-12-11 23:35:16 +0900

    [Bronze IV] Title: 영수증, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,26 @@
+'''
+준원이는 저번 주에 살면서 처음으로 코스트코를 가 봤다. 정말 멋졌다. 그런데, 몇 개 담지도 않았는데 수상하게 높은 금액이 나오는 것이다!
+준원이는 영수증을 보면서 정확하게 계산된 것이 맞는지 확인해보려 한다.
+
+영수증에 적힌,
+
+구매한 각 물건의 가격과 개수
+구매한 물건들의 총 금액
+을 보고, 구매한 물건의 가격과 개수로 계산한 총 금액이 영수증에 적힌 총 금액과 일치하는지 검사해보자.
+'''
+
+total_price = int(input())
+product_num = int(input())
+result = []
+for p in range(product_num):
+    price, num = map(int, input().split())
+    result.append(price*num)
+
+calculate_price = sum(result)
+if total_price == calculate_price:
+    print('Yes')
+else:
+    print('No')
+
+
+
